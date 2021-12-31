dd1dd258bf23147ce0aa0633a7c02f60506a35fd
motoshira <51407064+motoshira@users.noreply.github.com>
2021-12-31 17:57:30 +0900

    Solve 1-11

--- src/chapter1/1-1.scm
+++ src/chapter1/1-1.scm
@@ -1,16 +1,64 @@
 ;; 1-1
 
-(define (sqrt-iter guess x)
+;; 1-8 calc cube-root
+
+(define (cbrt-iter guess x)
   (if (good-enough? guess x)
       guess
-      (sqrt-iter (improve guess x)
+      (cbrt-iter (improve guess x)
                  x)))
 
 (define (good-enough? guess x)
-  (< (abs (- (* guess guess)
+  (< (abs (- (* guess guess guess)
              x))
      0.01))
 
 (define (improve guess x)
-  (/ (+ guess (/ x guess))
-     2))
+  (/ (+ (* guess 2)
+        (/ x guess guess))
+     3))
+
+;; 1-10
+
+(define (ackermann x y)
+  (cond
+   ((zero? y) 0)
+   ((zero? x) (* y 2))
+   ((= y 1) 2)
+   (else (ackermann (- x 1)
+                    (ackermann x (- y 1))))))
+
+(define (divide-count x)
+  (if (zero? (remainder x 2))
+      (+ (divide-count (quotient x 2))
+         1)
+      0))
+
+;; 1-11
+
+(define (f n)
+  (cond
+   ((< n 3) n)
+   (else
+    (+ (f (- n 1))
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3)))))))
+
+(define (f-iter-sub count a b c)
+  (if (zero? count)
+      a
+      (f-iter-sub (- count 1)
+                  (+ a
+                     (* 2 b)
+                     (* 3 c))
+                  a
+                  b)))
+
+(define (f-iter n)
+  (cond
+   ((< n 3) n)
+   (else
+    (f-iter-sub (- n 2)
+                2
+                1
+                0))))
