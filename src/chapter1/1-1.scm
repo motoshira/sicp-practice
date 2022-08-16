ceab6213bfd50b344db6dfea6af7d538e70518a7
motoshira <51407064+motoshira@users.noreply.github.com>
2022-08-16 16:22:24 +0900

    Solve 1-29

--- src/chapter1/1-1.scm
+++ src/chapter1/1-1.scm
@@ -202,3 +202,37 @@
                1)
             (miller-rabin-test n (- times 1))
             #f))))
+
+;; 1-29
+
+(define (inc x) (+ x 1))
+
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (* (sum f
+          (+ a (/ dx 2.0))
+          (lambda (x) (+ x dx))
+          b)
+     dx))
+
+(define (simpson-integral f a b n)
+  (unless (even? n)
+    (error "N must be even"))
+  (let ((h (/ (- b a) n)))
+    (define (g k)
+      (* (f (+ a (* k h)))
+         (if (odd? k)
+             4
+             2)))
+    (exact->inexact
+     (* (+ (sum g 1 inc n)
+           (g 0)
+           (g n))
+        (/ h 3)))))
