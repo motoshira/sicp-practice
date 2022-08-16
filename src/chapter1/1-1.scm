eec2ff5bb0b8d36ce3af033d46c29cb17ad2ca7e
motoshira <51407064+motoshira@users.noreply.github.com>
2022-08-16 16:41:31 +0900

    Solve 1-32

--- src/chapter1/1-1.scm
+++ src/chapter1/1-1.scm
@@ -288,3 +288,24 @@
       (lambda (x) (+ x 2))
       (* (+ n 1) 2))
      4))
+
+;; 1-32
+
+;; a
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a acc)
+    (if (> a b)
+        acc
+        (iter (next a)
+              (combiner acc
+                        (term a)))))
+  (iter a null-value))
+
+;; b
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
