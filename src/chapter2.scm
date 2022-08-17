ec526f4de7067d9c08f3b150438001ab192a9667
motoshira <51407064+motoshira@users.noreply.github.com>
2022-08-17 20:59:49 +0900

    Solve 2-3

--- src/chapter2.scm
+++ src/chapter2.scm
@@ -39,3 +39,61 @@
   (let ((mx (/ (+ (x-point p1) (x-point p2)) 2))
         (my (/ (+ (y-point p1) (y-point p2)) 2)))
     (make-point mx my)))
+
+;; 2-3
+
+;; 2つの線分で定義する
+
+(define (make-rect s1 s2)
+  (list s1 s2))
+
+(define (rect-s1 rect)
+  (car rect))
+
+(define (rect-s2 rect)
+  (cadr rect))
+
+(define (segment-length segment)
+  (let* ((start (start-segment segment))
+         (end (end-segment segment))
+         (dx (- (x-point start)
+                (x-point end)))
+         (dy (- (y-point start)
+                (y-point end))))
+    (sqrt (+ (* dx dx) (* dy dy)))))
+
+(define (rect-radius rect)
+  (* (+ (segment-length (rect-s1 rect))
+        (segment-length (rect-s2 rect)))
+     2))
+
+(define (rect-area rect)
+  (* (segment-length (rect-s1 rect))
+     (segment-length (rect-s2 rect))))
+
+;; 中点と
+
+(define (make-rectang p0 p1 p2 p3)
+  (list p0 p1 p2))
+
+(define (rectang-p0 rectang)
+  (car rectang))
+
+(define (rectang-p1 rectang)
+  (cadr rectang))
+
+(define (rectang-p2 rectang)
+  (caddr rectang))
+
+(define (rectang-radius rectang)
+  (* (+ (segment-length (make-segment (rectang-p0 rectang)
+                                      (rectang-p1 rectang)))
+        (segment-length (make-segment (rectang-p0 rectang)
+                                      (rectang-p2 rectang))))
+     2))
+
+(define (rectang-area rectang)
+  (* (segment-length (make-segment (rectang-p0 rectang)
+                                   (rectang-p1 rectang)))
+     (segment-length (make-segment (rectang-p0 rectang)
+                                   (rectang-p2 rectang)))))
