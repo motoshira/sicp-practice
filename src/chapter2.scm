a64cfbb1b277bb315de86647ec32082153b2afbe
motoshira <51407064+motoshira@users.noreply.github.com>
2022-08-19 22:04:12 +0900

    Solve 2-19

--- src/chapter2.scm
+++ src/chapter2.scm
@@ -287,3 +287,29 @@
         (rec (cdr xs)
              (cons (car xs) acc))))
   (rec xs '()))
+
+;; 2-19
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (first-denomiation coins)
+  (car coins))
+
+(define (except-first-denomiations coins)
+  (cdr coins))
+
+(define (no-more? coins)
+  (null? coins))
+
+(define (cc amount coin-values)
+  (cond
+   ((= amount 0) 1)
+   ((or (negative? amount)
+        (no-more? coin-values))
+    0)
+   (else
+    (+ (cc amount
+           (except-first-denomiations coin-values))
+       (cc (- amount (first-denomiation coin-values))
+           coin-values)))))
