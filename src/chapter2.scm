d1bc56cc43eb70885227d86c59d80b062d34125c
motoshira <51407064+motoshira@users.noreply.github.com>
2022-08-19 21:31:19 +0900

    Solve some problems

--- src/chapter2.scm
+++ src/chapter2.scm
@@ -250,3 +250,23 @@
                         (upper-bound y))
                      (* (lower-bound x)
                         (lower-bound y))))))))
+
+;; 2-12
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i))
+     2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i))
+     2))
+
+(define (make-center-percent c p)
+  (let ((w (* c p)))
+    (make-center-percent c w)))
+
+(define (percent i)
+  (/ (width i) (center i)))
